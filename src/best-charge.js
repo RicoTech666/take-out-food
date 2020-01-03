4e621d3c97049682ad383229fc18fc1e5d43b1e2
RicoTech666 <zhoujiachenbe@gmail.com>
2020-01-03 18:04:02 +0800

    [Jiachen] feat: add parse input function

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,18 @@
 function bestCharge(selectedItems) {
-  return /*TODO*/;
+  var allItems = loadAllItems();
+  var allPromotions = loadPromotions();
+
+  console.log("============= 订餐明细 =============");
+}
+
+function parseInput(selectedItems) {
+  const inputMap = new Map();
+  selectedItems.forEach(elem => {
+    inputMap.set(elem.split(" x ")[0],elem.split(" x ")[1]);
+  });
+  return inputMap;
+}
+
+function calculateOriginalPrice(inputItems) {
+
 }
