3c763c564cbfb1ac3556df702d4761b977b50f1b
RicoTech666 <zhoujiachenbe@gmail.com>
2020-01-03 20:41:36 +0800

    [Jiachen] feat: calculate original total price

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,7 +1,6 @@
 function bestCharge(selectedItems) {
-  var allItems = loadAllItems();
-  var allPromotions = loadPromotions();
-  var inputMap = parseInput(selectedItems);
+  var input = parseInput(selectedItems);
+  var soldFoodItems = getSoldFood(input);
   console.log("============= 订餐明细 =============");
 }
 
@@ -14,13 +13,22 @@ function parseInput(selectedItems) {
 }
 
 function getSoldFood(inputMap) {
+  var allItems = loadAllItems();
   var soldFoodItems = [];
   for (let [keyBarcode, valueFoodCount] of inputMap.entries()) {
     var foodItem = allItems.find(item => item.id === keyBarcode);
-    foodItem['count'] = valueFoodCount;
+    foodItem["count"] = valueFoodCount;
+    foodItem["totalPrice"] =
+      parseInt(foodItem.count) * parseInt(foodItem.price);
     soldFoodItems.push(foodItem);
   }
   return soldFoodItems;
 }
 
-
+function getOriginalTotalPrice(itemsToBeSummed) {
+  var originalTotalPrice = 0;
+  itemsToBeSummed.forEach(foodItem => {
+    originalTotalPrice += foodItem.totalPrice;
+  });
+  return originalTotalPrice;
+}
