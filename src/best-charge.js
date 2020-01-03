d6afb927383589065956feba1706881eedcd53d2
RicoTech666 <zhoujiachenbe@gmail.com>
2020-01-03 22:18:07 +0800

    [Jiachen] feat: print discount info

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,10 +1,11 @@
-var input = ["ITEM0013 x 4", "ITEM0022 x 1"];
+var input = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
 bestCharge(input);
 function bestCharge(selectedItems) {
   var input = parseInput(selectedItems);
   var soldFoodItems = getSoldFood(input);
   var originalTotalPrice = getOriginalTotalPrice(soldFoodItems);
-printEachFood(soldFoodItems);
+  printEachFood(soldFoodItems);
+  printPromotion(getPromotion(soldFoodItems, originalTotalPrice));
 }
 
 function parseInput(selectedItems) {
@@ -39,24 +40,31 @@ function getOriginalTotalPrice(itemsToBeSummed) {
 function getPromotion(itemsToBePromoted, originalTotalPrice) {
   const promotionThreshold = 30;
   var allPromotions = loadPromotions();
+  var discountedPrice = 0;
   var hasPromotions = false;
   var promotionType = "";
+  let discountedNameSet = new Set();
   itemsToBePromoted.forEach(foodItem => {
-    if (foodItem.id === allPromotions[1]["items"][0]) {
-      itemsToBePromoted.forEach(foodItemInner => {
-        if (foodItemInner.id === allPromotions[1]["items"][1]) {
-          hasPromotions = true;
-        }
-      });
+    if (allPromotions[1]["items"].includes(foodItem.id)) {
+      discountedPrice += parseInt(foodItem["totalPrice"]) / 2;
+      discountedNameSet.add(foodItem.name);
     }
   });
-  if (originalTotalPrice > promotionThreshold) {
-    hasPromotions = true;
-    promotionType = allPromotions[0]["type"];
-  } else if (hasPromotions) {
+  if (discountedPrice > 6) {
     promotionType = allPromotions[1]["type"];
+  } else if (discountedPrice < 6 && originalTotalPrice >= promotionThreshold) {
+    discountedPrice = 6;
+    promotionType = allPromotions[0]["type"];
+  } else if (
+    6 === discountedPrice &&
+    originalTotalPrice >= promotionThreshold
+  ) {
+    promotionType = allPromotions[0]["type"];
   }
-  return [hasPromotions, promotionType];
+  if (discountedPrice > 0) {
+    hasPromotions = true;
+  }
+  return [hasPromotions, promotionType, discountedPrice, discountedNameSet];
 }
 
 function printEachFood(itemsToBePrinted) {
@@ -68,12 +76,62 @@ function printEachFood(itemsToBePrinted) {
   });
 }
 
-function printPromotion(hasPromotions, promotionType) {
+function printPromotion([
+  hasPromotions,
+  promotionType,
+  discountedPrice,
+  discountedNameSet
+]) {
   if (!hasPromotions) {
     return;
   }
+
+  console.log("-----------------------------------");
+  console.log("使用优惠：");
   var allPromotions = loadPromotions();
+
   if (promotionType === allPromotions[0]["type"]) {
+    console.log(`${promotionType}，省${discountedPrice}元`);
   } else if (promotionType === allPromotions[1]["type"]) {
+    console.log(
+      `${promotionType}(${[...discountedNameSet]})，省${discountedPrice}元`
+    );
   }
+}
+
+function loadAllItems() {
+  return [
+    {
+      id: "ITEM0001",
+      name: "黄焖鸡",
+      price: 18.0
+    },
+    {
+      id: "ITEM0013",
+      name: "肉夹馍",
+      price: 6.0
+    },
+    {
+      id: "ITEM0022",
+      name: "凉皮",
+      price: 8.0
+    },
+    {
+      id: "ITEM0030",
+      name: "冰锋",
+      price: 2.0
+    }
+  ];
+}
+
+function loadPromotions() {
+  return [
+    {
+      type: "满30减6元"
+    },
+    {
+      type: "指定菜品半价",
+      items: ["ITEM0001", "ITEM0022"]
+    }
+  ];
 }
