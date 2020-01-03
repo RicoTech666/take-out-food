6d38cc38db0d1696e69e824d2ed59a1e868e70d4
RicoTech666 <zhoujiachenbe@gmail.com>
2020-01-03 20:25:47 +0800

    [Jiachen] feat: transfer sold food into an object array

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,18 +1,26 @@
 function bestCharge(selectedItems) {
   var allItems = loadAllItems();
   var allPromotions = loadPromotions();
-
+  var inputMap = parseInput(selectedItems);
   console.log("============= 订餐明细 =============");
 }
 
 function parseInput(selectedItems) {
   const inputMap = new Map();
   selectedItems.forEach(elem => {
-    inputMap.set(elem.split(" x ")[0],elem.split(" x ")[1]);
+    inputMap.set(elem.split(" x ")[0], elem.split(" x ")[1]);
   });
   return inputMap;
 }
 
-function calculateOriginalPrice(inputItems) {
-
+function getSoldFood(inputMap) {
+  var soldFoodItems = [];
+  for (let [keyBarcode, valueFoodCount] of inputMap.entries()) {
+    var foodItem = allItems.find(item => item.id === keyBarcode);
+    foodItem['count'] = valueFoodCount;
+    soldFoodItems.push(foodItem);
+  }
+  return soldFoodItems;
 }
+
+
